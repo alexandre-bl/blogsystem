cd8c2b03da07a7c8ee5c53c81db1b6ff6f64c276
Alexandre B. L <alexandrebl@protonmail.com>
2021-02-04 21:17:47 +0000

    04/02/2021 21:17:47

--- index.php
+++ index.php
@@ -6,6 +6,6 @@ $posts = json_decode( $posts, true );
 $config = file_get_contents( "config.json" );
 $config = json_decode( $config, true );
 
-require_once "themes/".$config["theme"].".php";
+require_once "themes/".$config["theme"]."/index.php";
 
 ?>
