4d8bbd6348d4c63a62ad03afbde7282bf2322215
Alexandre B. L <alexandrebl@protonmail.com>
2021-02-04 23:05:40 +0000

    04/02/2021 23:05:40

--- themes/default/index.php
+++ themes/default/index.php
@@ -11,45 +11,74 @@
 
     <body>
     
-        <h1 id="page_title"> <?php echo $content["title"]; ?> </h1>
+        <?php if( empty( $_GET["p"] ) ) { ?>
 
-        <div id="links"> <?php
+            <h1 id="page_title"> <?php echo $content["title"]; ?> </h1>
 
-            echo "<a href='/'>Home</a>";
+            <div id="links"> <?php
 
-            foreach( $content["links"] as $link ) {
+                echo "<a href='/'>Home</a>";
 
-                $label = $link["label"];
-                $url   = $link["url"];
+                foreach( $content["links"] as $link ) {
 
-                echo "<a href='$url'>$label</a>";
+                    $label = $link["label"];
+                    $url   = $link["url"];
 
-            }
-        ?> </div>
+                    echo "<a href='$url'>$label</a>";
 
-        <p id="page_desc"> <?php echo $content["description"]; ?> </p>
-    
-        <table id="posts">
-            <tr>
-                <th>Post Title</th>
-                <th>Post Date</th>
-            </tr>
-            <?php
-
-                foreach( $content["posts"] as $post ) {
-                    echo "<tr>";
-
-                    $title = $post["title"];
-                    $date = $post["date"];
-
-                    echo "<td>$title</td>";
-                    echo "<td>$date</td>";
-
-                    echo "</tr>";
                 }
+            ?> </div>
+
+            <p id="page_desc"> <?php echo $content["description"]; ?> </p>
+        
+            <table id="posts">
+                <tr>
+                    <th>Post Title</th>
+                    <th>Post Date</th>
+                </tr>
+                <?php
+
+                    foreach( $content["posts"] as $post ) {
+                        echo "<tr>";
+
+                        $title = $post["title"];
+                        $date = $post["date"];
+                        $url = "/?p=".$post["file"];
+
+                        echo "<td>$title</td>";
+                        echo "<td>$date</td>";
+
+                        echo "</tr>";
+                    }
+
+                ?>
+            </table>
+
+        <?php } else {
+
+            $post = $content["posts"][$_GET["p"]];
 
             ?>
-        </table>
+
+            <h1 id="page_title"> <?php echo $post["title"]; ?> </h1>
+
+            <div id="links"> <?php
+
+                echo "<a href='/'>Home</a>";
+
+                foreach( $content["links"] as $link ) {
+
+                    $label = $link["label"];
+                    $url   = $link["url"];
+
+                    echo "<a href='$url'>$label</a>";
+
+                }
+            ?> </div>
+
+            <p id="page_desc"> <?php echo file_get_contents( "uploads/posts/".$_GET["p"].".md" ); ?> </p>
+
+        <?php } ?> 
 
     </body>
 
