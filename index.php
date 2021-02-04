c704d70d617c5ddcf13ed60eb86dfdcdada2a8b5
Alexandre B. L <alexandrebl@protonmail.com>
2021-02-04 21:55:08 +0000

    04/02/2021 21:55:08

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 
-    $content = file_get_contents( "posts.json" );
+    $content = file_get_contents( "content.json" );
     $content = json_decode( $content, true );
 
     $theme_dir = "themes/".$content["theme"];
-
-    echo $theme_dir;
 
     require_once $theme_dir."/index.php";
 
