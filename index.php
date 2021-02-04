1cc46da9135cb1996f1d37c471679db8cdaf1a86
Alexandre B. L <alexandrebl@protonmail.com>
2021-02-04 21:52:56 +0000

    04/02/2021 21:52:56

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 
-$content = file_get_contents( "posts.json" );
-$content = json_decode( $content, true );
+    $content = file_get_contents( "posts.json" );
+    $content = json_decode( $content, true );
 
-$theme_dir = "themes/".$content["theme"];
+    $theme_dir = "themes/".$content["theme"];
 
-require_once $theme_dir."/index.php";
+    require_once $theme_dir."/index.php";
+
+    echo "test";
 
 ?>
