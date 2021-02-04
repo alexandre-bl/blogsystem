1419dd914fd166d0ad73299ca0b9d62c7d36b207
Alexandre B. L <alexandrebl@protonmail.com>
2021-02-04 21:14:54 +0000

    04/02/2021 21:14:54

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 $posts = file_get_contents( "posts.json" );
 $posts = json_decode( $posts, true );
 
-print_r( $posts );
+$config = file_get_contents( "config.json" );
+$config = json_decode( $config, true );
 
-require_once "themes/default.php";
+require_once "themes/".$config["theme"].".php";
 
 ?>
