ebb17d139b918f99c43a032132d7aa93881f9162
Alexandre B. L <alexandrebl@protonmail.com>
2021-02-04 19:23:28 +0000

    04/02/2021 19:23:28

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once "config.php";
+
+$mysql = new mysqli($server, $user, $password);
+
+$mysql->query( "
+    CREATE TABLE IF NOT EXISTS posts ( title TEXT, content TEXT, date INT )
+" );
+
+$posts = $mysql->query("SELECT * from posts");
+
+print_r( $posts );
+
+require_once "theme/".$theme.".php";
+
+$mysql->close();
