d57625a81f37abe738ca51d1e18a8e8d728827a9
Alexandre B. L <alexandrebl@protonmail.com>
2021-02-05 18:55:12 +0000

    05/02/2021 18:55:12

--- admin/login.php
+++ admin/login.php
@@ -1,43 +1,42 @@
-<?php
-
-error_reporting( 8 );
-
-require_once "users.php";
-
-$content = file_get_contents( "../content.json" );
-$content = json_decode( $content, true );
-$returning = false;
-
-if( isset( $_POST["user"] ) ) {
-
-    if( array_key_exists( $_POST["user"], $users ) ) {
-
-        if( $users[$_POST["user"]] == $_POST["password"] ) {
-
-            //setcookie( "user", "alex" );
-            header('Location: /admin/', true, 303);
-            exit();
-
-        } else {
-
-            $returning = true;
-
-        }
-
-    } else {
-
-        $returning = true;
-
-    }
-
-}
-
-?>
-
 <!DOCTYPE html>
 <html>
 
     <head>
+
+        <?php
+
+            error_reporting( 8 );
+
+            require_once "users.php";
+
+            $content = file_get_contents( "../content.json" );
+            $content = json_decode( $content, true );
+            $returning = false;
+
+            if( isset( $_POST["user"] ) ) {
+
+                if( array_key_exists( $_POST["user"], $users ) ) {
+
+                    if( $users[$_POST["user"]] == $_POST["password"] ) {
+
+                        setcookie( "user", $_POST["user"] );
+                        echo "<script>location.href='/admin/';</script>";
+
+                    } else {
+
+                        $returning = true;
+
+                    }
+
+                } else {
+
+                    $returning = true;
+
+                }
+
+            }
+
+        ?>
 
         <link rel="stylesheet" href="<?php echo "../themes/".$content["theme"]."/universal.css" ; ?>">
         <link rel="stylesheet" href="style.css">
